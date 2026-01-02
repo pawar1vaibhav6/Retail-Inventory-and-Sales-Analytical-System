50a062978d06bc6d8fcf6951e289b16a2db90c34
Vaibhav Pawar <pawar1vaibhav6@gmail.com>
2026-01-02 18:10:50 +0530

    Changes in Tables

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,4 +1,4 @@
 create table Products(product_id int primary key,p_name varchar(20),category varchar(20),price int,stock_quantity int);
-create table Customers(customer_id int primary key,c_name varchar(20),email varchar(25),region varchar);
+create table Customers(customer_id int primary key Identity,c_name varchar(20),email varchar(25),region varchar);
 create table Sales(sales_id int primary key,product_id int foreign key references products(product_id),customer_id int foreign key references Customers(customer_id),
 				Date date,quantity int,total_amount int);
