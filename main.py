ad97a28541b2f712a1b032536bfa5a54fdb9c1e7
Vaibhav Pawar <pawar1vaibhav6@gmail.com>
2026-01-02 18:02:46 +0530

    Added main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import Product_insertion as pi
+
+def main():
+    print(
+        "1.Add New Product\n"
+          "2.Update Price of Existing Product\n"
+          "3.Update Stock of Existing Product\n"
+          "4.Exit"
+          )
+    try:
+        options=int(input("Choose From above(1,2,3,4):"))
+    except ValueError:
+        print("Enter a valid number")
+        return
+    if options==1:
+        pi.new_product()
+    elif options==2:
+        pi.price_increase()
+    elif options==3:
+        pi.stock_increase()
+    elif options==4:
+        print("Thank you for visiting")
+    else:
+        print("Invalid Input")
+
+if __name__=="__main__":
+    main()
+
+
