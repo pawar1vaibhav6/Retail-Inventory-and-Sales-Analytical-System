cb40d004212eccabbf8b508c9509a067e278e9da
Vaibhav Pawar <pawar1vaibhav6@gmail.com>
2026-01-03 08:58:52 +0530

    Change Date to Datetime

--- SQLQuery.sql
+++ SQLQuery.sql
@@ -1,4 +1,4 @@
 create table Products(product_id int primary key,p_name varchar(20),category varchar(20),price int,stock_quantity int);
 create table Customers(customer_id int primary key,c_name varchar(20),email varchar(25) unique,region varchar (20));
 create table Sales(sales_id int primary key identity,product_id int foreign key references products(product_id),customer_id int foreign key references Customers(customer_id),
-				Date date,quantity int,total_amount int);
+				Date datetime,quantity int,total_amount int);
